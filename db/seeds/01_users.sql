234cf413af2abe61507ebedce01bb82577ac389b
Sidney Pang <sidneypang@hotmail.co.uk>
2022-07-02 05:33:01 +0000

    created tables and filled with data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+INSERT INTO users (name, password, profile_icon) VALUES ('Alice', 'password', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png');
+INSERT INTO users (name, password, profile_icon) VALUES ('Derek', 'password', 'https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png');
+INSERT INTO users (name, password, profile_icon) VALUES ('Andy', 'password', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png');
+INSERT INTO users (name, password, profile_icon) VALUES ('Janet', 'password', 'https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png');
+INSERT INTO users (name, password, profile_icon) VALUES ('Jessica', 'password', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png');
