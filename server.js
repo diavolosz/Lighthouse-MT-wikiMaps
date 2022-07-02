e4f2be286dee6c021910b62a43f61b424ae4972d
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-07-02 03:32:46 +0000

    added /welcome get route for rendering welcome.ejs page

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/welcome", (req, res) => {
+  res.render("welcome");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
