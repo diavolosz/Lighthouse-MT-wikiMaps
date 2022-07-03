affbce18a5e879aff78af39afbe4be11055037b2
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-07-03 17:54:29 +0000

    add mechanism where upon click of marker will display popup chatbox

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,5 +1,8 @@
 
-let map = L.map('map').setView([43.666707, -79.391248], 15);
+let initialMapCentre = [43.663937824894184, -79.39198578896757]
+
+
+let map = L.map('map').setView(initialMapCentre, 16);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -9,11 +12,66 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
-const myIcon = L.icon({
+const pinIcon = L.icon({
   iconUrl: '/image/pinIcon.png',
   iconSize: [60, 60]
 })
 
-let marker = L.marker([43.665, -79.386], {icon: myIcon}).addTo(map);
+
+
+// temperory marker location database object
+
+let locations = {
+  pin1: {
+    lat:43.66628167959075,
+    long:-79.39462508254417
+  },
+  pin2: {
+    lat:43.66446558824718,
+    long:-79.39230765399577
+  },
+  pin3: {
+    lat:43.66219151608988,
+    long:-79.38661064209391
+  },
+  pin4: {
+    lat:43.66282019311965,
+    long:-79.39571942382528
+  },
+  pin5: {
+    lat:43.66614974316886,
+    long:-79.39622367911178
+  }
+}
+
+
+
+//function to show popup
+let popup = L.popup();
+
+function onMapClick(pin) {
+  popup
+  .setLatLng(pin.latlng)
+  .setContent("You clicked the map at " + pin.latlng.toString())
+  .openOn(map);
+}
+
+
+// helper function to generate pin display on map from database
+const generatePinOnMap = function(database) {
+  for (let eachPin in database) {
+    let lat = database[eachPin].lat
+    console.log(lat)
+    let long = database[eachPin].long
+    console.log(long)
+    marker = new L.marker([lat, long], {icon: pinIcon})
+      .addTo(map);
+    marker.on('click', onMapClick);
+    }
+  return
+}
+generatePinOnMap(locations)
+
+
 
 
