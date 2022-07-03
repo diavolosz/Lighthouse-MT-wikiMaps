98adb87260c024e3337d8b2a290356bbbf6f8b21
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-07-03 17:28:54 +0000

    updated icon usage on map display

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,7 +1,19 @@
 
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([43.666707, -79.391248], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+
+
+
+const myIcon = L.icon({
+  iconUrl: '/image/pinIcon.png',
+  iconSize: [60, 60]
+})
+
+let marker = L.marker([43.665, -79.386], {icon: myIcon}).addTo(map);
+
+
