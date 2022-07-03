4ead55eb7fc2f82d299ed28b4bdb0c57839e896d
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-03 13:36:18 -0400

    Modularize maps router

--- server.js
+++ server.js
@@ -37,11 +37,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const mapRoutes = require("./routes/maps");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/maps/new", mapRoutes);
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -50,10 +53,6 @@ app.use("/api/widgets", widgetsRoutes(db));
 
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/maps/new", (req, res) => {
-  res.render("maps_new");
 });
 
 app.listen(PORT, () => {
