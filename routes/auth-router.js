4b2985c9c919de345fa100c826abb65df3073626
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-05 01:18:05 -0400

    Implement authenication

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require("bcryptjs"); // Hashing library
+
 const user_helpers = require("../db/user_helpers");
 
 router.get("/", (req, res) => {
@@ -7,14 +9,25 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const { email, password } = req.body;
+  const { email } = req.body;
+  const formPassword = req.body.password;
+  let id;
 
   user_helpers.getUserWithEmail(email).then((user) => {
-    if (user.password === password) {
-      return res.redirect('/user/1')
-    } else {
+    if (!user) {
       return res.send('Invalid credentials');
     }
+
+    id = user.id;
+    const dbPassword = user.password;
+
+    if (!(bcrypt.compareSync(formPassword, dbPassword))) {
+      return res.send('Invalid credentials');
+    }
+
+    req.session.user_id = id;
+    return res.redirect(`/user/${id}`);
+
   }).catch((err) => {
     console.log(err)
     res.send("Invalid credentials.");
