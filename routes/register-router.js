e3f4497f5daa51761f8cd8dd1ed244ec5971a4c1
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-05 01:17:31 -0400

    Implement user registration

--- routes/register-router.js
+++ routes/register-router.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const router  = express.Router();
-const user_helpers = require("../db/user_helpers");
+const bcrypt = require("bcryptjs"); // Hashing library
 
+const user_helpers = require("../db/user_helpers"); // Database helper queries
 
-//render register webpage from welcome page sign up
+// Render register page
 router.get("/", (req, res) => {
   res.render("register");
 });
 
-// update database with user info
+// Register user
 router.post("/", (req, res) => {
 
   // Extract form data
-  const { name, email, password } = req.body;
+  const { name, email } = req.body;
+  const formPassword = req.body.password;
 
   // Check if username already exists in database
   user_helpers.getUserWithName(name).then((result) => {
-    if (!result) {
-      // Result should be undefined if name is not registered in database
 
-      user_helpers.getUserWithEmail(email).then((result) => {
+    if (!result) { // Check if email already exists in database
+    user_helpers.getUserWithEmail(email).then((result) => {
 
-        if (!result) {
-          //Result should be undefined if email is not registered in database
-          //Proceed to update data with both passed username + email
-
-          user_helpers.addUser({ name, email, password })
-          .then(() => {
-            return res.redirect('/')
-          });
-        } else {
-          // Email exists, registration should fail
+      if (!result) {
+        const password = bcrypt.hashSync(formPassword, 10); // Hashed password
+        user_helpers.addUser({ name, email, password })
+        .then((user) => {
+          req.session.user_id = user.id;
+          return res.redirect('/')
+        });
+      } else {
+        // Send error if email already exists in database
           return res.send("That email address is unavailable.");
         }
       });
     } else {
-      // Username exists, registration should fail
+      // Send error if username already exists in database
       return res.send("Username taken.");
     }
   });
