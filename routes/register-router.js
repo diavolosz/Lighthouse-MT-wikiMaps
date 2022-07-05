1a28783436090fb1b853858b87c6c85d65aeee16
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-05 03:32:59 -0400

    Render error if username contains whitespace

--- routes/register-router.js
+++ routes/register-router.js
@@ -21,13 +21,19 @@ router.post("/", (req, res) => {
 
   // Extract form data
   let { name, email } = req.body;
-  name = name.trim().replace(/ /g,'').toLowerCase();
   email = email.trim();
   const formPassword = req.body.password;
 
   if (!name || !email || !formPassword) {
     return res.render("register", { error: "Please fill in all fields." });
   }
+
+  if (name.match(/\s/g) !== -1) {
+    return res.render("register", { error: "Username cannot contain spaces." });
+  }
+
+  name = name.trim().replace(/ /g, '').toLowerCase();
+
 
   // Check if username already exists in database
   user_helpers.getUserWithName(name).then((result) => {
