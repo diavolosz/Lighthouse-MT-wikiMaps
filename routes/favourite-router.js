00a6d33a7e667f7ff5b3dcf8b8dffdb9d6e38fcf
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-05 23:31:35 -0400

    Basic route handler for favourite/:id

---
+++ routes/favourite-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const userQuery = require('../db/user_helpers');
+
+router.get("/:id", (req, res) => {
+    /**
+     * 1. check if logged in and extract user id
+     * 2. extract param
+     * 3. validate param
+     * 4. if user has already favorited, delete
+     * 5. if user has not favorited, insert
+     */
+
+  let id = req.session.user_id;
+  let mapId = req.params.id;
+
+  userQuery.getUserWithID(id).then((result) => {
+  if (!result) {
+    return res.send("You must login.");
+  }
+  console.log(mapId)
+  res.send(mapId);
+  });
+
+});
+
+module.exports = router;
+
